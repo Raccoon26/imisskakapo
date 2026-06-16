4af893dd88e5744e895d585b955f64bee7959a17
Kawauso <85695743+Raccoon26@users.noreply.github.com>
2026-06-16 13:52:07 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const graduationDate =
+new Date("2026-06-16T00:00:00+09:00");
+
+function updateCounter(){
+
+    const now = new Date();
+
+    const diff =
+    now - graduationDate;
+
+    const days =
+    Math.floor(
+        diff /
+        (1000 * 60 * 60 * 24)
+    );
+
+    const hours =
+    Math.floor(
+        diff /
+        (1000 * 60 * 60)
+    ) % 24;
+
+    const minutes =
+    Math.floor(
+        diff /
+        (1000 * 60)
+    ) % 60;
+
+    const seconds =
+    Math.floor(
+        diff /
+        1000
+    ) % 60;
+
+    document.getElementById("counter")
+    .textContent =
+
+    `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+
+}
+
+updateCounter();
+
+setInterval(
+    updateCounter,
+    1000
+);
